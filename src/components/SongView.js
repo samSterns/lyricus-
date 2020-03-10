63f519e672acb21b42f8d0f4e393b5f9e5b82744
samSterns <sam.g.sterns@gmail.com>
2020-03-10 13:48:30 -0700

    songs rendering again packages current

--- src/components/SongView.js
+++ src/components/SongView.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import useSong from '../hooks/useSong.js';
-import TagCloud from 'react-tag-cloud';
 
 const SongView = () => {
   const { song } = useSong();
 
-
-
-
   return (
-    <TagCloud 
-      style={{
-        fontFamily: 'sans-serif',
-        fontSize: 30,
-        fontWeight: 'bold',
-        fontStyle: 'italic',
-        padding: 5,
-        width: '100%',
-        height: '100%'
-      }}> 
-      <div style={{ fontSize: 50 }}>react</div>
-      <div style={{ color: 'green' }}>tag</div>
-      <div rotate={90}>cloud</div>
-      <p>{song}</p>
-    </TagCloud>
+
+    <p>{song}</p>
+
   );
 };
 
