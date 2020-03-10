fe39d67254523b8fe46d7e1ec8d00a95d5f2de7d
samSterns <sam.g.sterns@gmail.com>
2020-03-10 13:43:09 -0700

    fun tangent. take it bake to the old way and format lyrics regularly and use npm package for personal site

--- src/components/SongView.js
+++ src/components/SongView.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import useSong from '../hooks/useSong.js';
-
+import TagCloud from 'react-tag-cloud';
 
 const SongView = () => {
   const { song } = useSong();
 
-  // const cloud = WordCloud(document.getElementById({ song }), { ['song', 6, {song} ]});
+
 
 
   return (
-    <>
+    <TagCloud 
+      style={{
+        fontFamily: 'sans-serif',
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        padding: 5,
+        width: '100%',
+        height: '100%'
+      }}> 
+      <div style={{ fontSize: 50 }}>react</div>
+      <div style={{ color: 'green' }}>tag</div>
+      <div rotate={90}>cloud</div>
       <p>{song}</p>
-
-    </>
+    </TagCloud>
   );
 };
 
