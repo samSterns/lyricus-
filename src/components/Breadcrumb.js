a99092820651dae6870dac58a7bcad5b5fe134a3
samSterns <sam.g.sterns@gmail.com>
2020-03-13 17:11:00 -0700

    pagination at bottom

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -7,6 +7,7 @@ import HomeIcon from '@material-ui/icons/Home';
 import AlbumIcon from '@material-ui/icons/Album';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import QueueMusicIcon from '@material-ui/icons/QueueMusic';
+import styles from './Breadcrumb.css';
 
 const useStyles = makeStyles(theme => ({
   link: {
@@ -21,34 +22,43 @@ const useStyles = makeStyles(theme => ({
 
 function handleClick(event) {
   event.preventDefault();
-  console.info('You clicked a breadcrumb.');
 }
 
 export default function IconBreadcrumbs() {
   const classes = useStyles();
 
+  
+
   return (
-    <Breadcrumbs aria-label="breadcrumb">
+    <Breadcrumbs className={styles.Breadcrumbs} aria-label="breadcrumb">
       <Link color="inherit" href="/" onClick={handleClick} className={classes.link}>
         <HomeIcon className={classes.icon} />
         Artist Search
       </Link>
       <Link
         color="inherit"
-        href="/getting-started/installation/"
+        href="/"
         onClick={handleClick}
         className={classes.link} >
         <AlbumIcon className={classes.icon} />
         Album
       </Link>
-      <Typography color="textPrimary" className={classes.link}>
+      <Link
+        color="inherit"
+        href="/getting-started/installation/"
+        onClick={handleClick}
+        className={classes.link} >
         <MusicNoteIcon className={classes.icon} />
         Songs
-      </Typography>
-      <Typography color="textPrimary" className={classes.link}>
+      </Link>
+      <Link
+        color="inherit"
+        href="/getting-started/installation/"
+        onClick={handleClick}
+        className={classes.link} >
         <QueueMusicIcon className={classes.icon} />
         Lyrics
-      </Typography>
+      </Link>
     </Breadcrumbs>
   );
 }
